08b3e6e9d6d27bcde817bc5f92a7dbf9113478e1
Brett Bailey <bretto36@gmail.com>
2025-07-30 08:58:02 +1000

    Remove support for hyperlinks as it was implemented very poorly

--- src/Html/Font.php
+++ src/Html/Font.php
@@ -4,20 +4,29 @@ namespace RtfHtmlPhp\Html;
 
 class Font
 {
-  public $family;
-  public $name;
-  public $charset;
-  public $codepage;
-  public $fprq;
-  
-  public function toStyle(): string {
-    $list = array();
-    // If the name is in double quotes replace them with single quotes
-    if($this->name) $this->name = preg_replace("/^\"(.*)\"$/", '\'$1\'', $this->name);
-    if($this->name) $list[] = $this->name;
-    if($this->family) $list[] = $this->family;
-    if(sizeof($list) == 0) return "";
-    return "font-family:" . join(', ', $list) . ";";
-  }
+    public $family;
+    public $name;
+    public $charset;
+    public $codepage;
+    public $fprq;
+
+    public function toStyle(): string
+    {
+        $list = [];
+        // If the name is in double quotes replace them with single quotes
+        if ($this->name) {
+            $this->name = preg_replace("/^\"(.*)\"$/", '\'$1\'', $this->name);
+        }
+        if ($this->name) {
+            $list[] = $this->name;
+        }
+        if ($this->family) {
+            $list[] = $this->family;
+        }
+        if (sizeof($list) == 0) {
+            return "";
+        }
+        return "font-family:" . join(', ', $list) . ";";
+    }
 }
 
